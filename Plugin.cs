dc71351a14ed40109fe67003e951059980dba588
ozen-m <74519617+ozen-m@users.noreply.github.com>
2025-11-13 23:50:59 +0800

    slnx, csproj, build files + .gitignore + Up v

--- Plugin.cs
+++ Plugin.cs
@@ -9,7 +9,7 @@ using UnityEngine;
 
 namespace ContinuousLoadAmmo
 {
-    [BepInPlugin("com.ozen.continuousloadammo", "ContinuousLoadAmmo", "1.1.0")]
+    [BepInPlugin("com.ozen.continuousloadammo", "ContinuousLoadAmmo", "1.1.1")]
     [BepInDependency("Tyfon.UIFixes", BepInDependency.DependencyFlags.SoftDependency)]
     public class Plugin : BaseUnityPlugin
     {
