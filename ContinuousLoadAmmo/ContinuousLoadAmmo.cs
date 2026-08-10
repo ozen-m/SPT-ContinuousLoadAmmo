2bbe37c1f18f3b89343698be15a6d55bd8089b7d
ozen-m <74519617+ozen-m@users.noreply.github.com>
2026-08-10 13:47:15 +0800

    Move to project folder

--- ContinuousLoadAmmo/ContinuousLoadAmmo.cs
+++ ContinuousLoadAmmo/ContinuousLoadAmmo.cs
@@ -12,7 +12,6 @@ namespace ContinuousLoadAmmo;
 
 [BepInPlugin("com.ozen.continuousloadammo", "Continuous Load Ammo", "1.1.6")]
 [BepInDependency("com.tyfon.uifixes", BepInDependency.DependencyFlags.SoftDependency)]
-[BepInDependency("Tyfon.UIFixes", BepInDependency.DependencyFlags.SoftDependency)] // Old version; TODO: Remove in 4.1.x
 [SuppressMessage("Usage", "CA2211:Non-constant fields should not be visible")]
 public class ContinuousLoadAmmo : BaseUnityPlugin
 {
